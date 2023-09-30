95aeed940cca7ccba5c9b99300447db454c98c78
Peter Tiewah <petertiewah@MBP-XFYH6CDJCT-TIEWAH.local>
2023-09-30 11:50:08 -0400

    Initial commit

---
+++ app/src/main/java/com/example/project2/ui/rooms/RoomsFragment.kt
@@ -0,0 +1,44 @@
+package com.example.project2.ui.rooms
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.project2.databinding.FragmentRoomsBinding
+import com.example.project2.ui.people.PeopleViewModel
+
+
+class RoomsFragment : Fragment() {
+
+    private var _binding: FragmentRoomsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val roomsFragment =
+            ViewModelProvider(this).get(RoomsViewModel::class.java)
+
+
+        _binding = FragmentRoomsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+//        val textView: TextView = binding.textDashboard
+//        dashboardViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
